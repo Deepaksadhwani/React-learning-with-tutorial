3bfcccd422868d15cbbf16ae4b55f0d682c5707c
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-02-17 21:15:36 +0530

    modified some files and added expenses.js & css

--- react tutorial 1/my-app/src/components/ExpenseItem.js
+++ react tutorial 1/my-app/src/components/ExpenseItem.js
@@ -1,24 +1,25 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExepenseDetails from "./ExpenseDetails";
+import Card from "./Card"
 
 
 function ExpenseItem(props) {
 
     return (
-        <div className="expense-item">
+        <Card className = "expense-item">
 
-            <ExepenseDetails title={props.title} className="expense-item__description" />
-            <ExpenseDate date={props.date}></ExpenseDate>
-            <ol>
-                <li className="expense-item__price"> <ExepenseDetails amount={props.amount} /></li>
-                <li>{props.location}</li>
-            </ol>
+                < ExepenseDetails title = { props.title } className = "expense-item__description" />
+                <ExpenseDate date={props.date}></ExpenseDate>
+                <ol>
+                    <li className="expense-item__price"> <ExepenseDetails amount={props.amount} /></li>
+                    <li>{props.location}</li>
+                </ol>
 
 
 
-        </div >
-    )
+            </ Card>
+        )
 }
 
 export default ExpenseItem;
---
+++ react tutorial 1/my-app/src/components/expenses.js
@@ -0,0 +1,48 @@
+import "./expenses.css"
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+
+function Expenses() {
+    const expense = [
+        {
+            expenseDate: new Date(2021, 2, 28),
+            title: "Car Insurance",
+            expenseAmount: 294.65,
+            locationOfExpenditure: "Rajasthan"
+        },
+        {
+            expenseDate: new Date(2021, 7, 16),
+            title: "Laptop Purchase",
+            expenseAmount: 45000,
+            locationOfExpenditure: "Office"
+        },
+
+        {
+            expenseDate: new Date(2022, 6, 22),
+            title: "Furniture",
+            expenseAmount: 10000,
+            locationOfExpenditure: "Home"
+        },
+
+        {
+            expenseDate: new Date(2021, 10, 14),
+            title: "Washing machine",
+            expenseAmount: 30000,
+            locationOfExpenditure: "mumbai"
+        },
+
+    ];
+    const expenseslist = []
+    for (let i = 0; i < expense.length; i++) {
+        expenseslist.push(<ExpenseItem date={expense[i].expenseDate} title={expense[i].title} amount={expense[i].expenseAmount}
+            location={expense[i].locationOfExpenditure}></ExpenseItem>)
+    }
+
+    return (
+        <Card className="expenses">
+            {expenseslist}
+        </Card>
+    )
+}
+
+export default Expenses;
