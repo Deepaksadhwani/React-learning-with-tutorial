021b01259c599b8b64a9dfb9df9b1a917bb8910c
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-02-23 16:41:54 +0530

    applied useState in ExpenseItem.js

--- React Tutorial/my-app/src/components/Expenses/ExpenseItem.js
+++ React Tutorial/my-app/src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExepenseDetails from "./ExpenseDetails";
@@ -6,18 +7,22 @@ import Card from "../UI/Card"
 
 const ExpenseItem = (props) => {
 
+    const [amount, setAmount] = useState(props.amount);
+    const [title, setTitle] = useState(props.title)
 
     const deleteHandler = () => {
-        console.log("Expense deleted");
+        setAmount(100 + `$`);
+        setTitle("Updated!")
+
     };
 
     return (
         <Card className="expense-item">
 
-            < ExepenseDetails title={props.title} className="expense-item__description" />
+            < ExepenseDetails title={title} className="expense-item__description" />
             <ExpenseDate date={props.date}></ExpenseDate>
             <ol>
-                <li className="expense-item__price"> <ExepenseDetails amount={props.amount} /></li>
+                <li className="expense-item__price"> <ExepenseDetails amount={amount} /></li>
                 <li>{props.location}</li>
             </ol>
             <delete onClick={deleteHandler}>Delete</delete>
