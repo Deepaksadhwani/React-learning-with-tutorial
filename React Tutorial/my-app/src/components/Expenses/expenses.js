19d3803a6d79d46b88a84aa9201e36a3d3f7f845
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-02-28 08:14:47 +0530

    added chart and chartBar

--- React Tutorial/my-app/src/components/Expenses/expenses.js
+++ React Tutorial/my-app/src/components/Expenses/expenses.js
@@ -3,7 +3,7 @@ import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
-
+import ExpensesChart from "./ExpensesChart";
 const Expenses = (props) => {
 
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -21,6 +21,7 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList items={filteredExpenses} />
         </Card>
     )
